d9f3598467037c1e9b57c08fc75b8f4b9d48d97c
Your Name <you@example.com>
2025-01-07 14:34:36 +0530

    Java Programe

---
+++ oprator.java
@@ -0,0 +1,20 @@
+public class oprator {
+    public static void main(String[] args) {
+        int m = 24;
+        int n = 45;
+        System.out.println(m>n);
+        System.out.println(25>=50);
+        System.out.println(25<=50);
+        System.out.println(n>m && 'A'==65);
+        System.out.println(30>50 || 'z'==19);
+        System.out.println(true==false);
+        System.out.println(false!= false);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
